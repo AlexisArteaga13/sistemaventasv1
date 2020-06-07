82f814376886da51fb572a033639e4f78a138fd2
hector15933 <hectorxdtom@gmail.com>
2020-06-07 01:10:11 -0500

    123

--- config/global.php
+++ config/global.php
@@ -9,7 +9,7 @@ define("DB_NAME", "basedeventas");
 define("DB_USERNAME", "root");
 
 //Contraseña del usuario de la base de datos
-define("DB_PASSWORD", "alexis1303");
+define("DB_PASSWORD", "");
 
 //definimos la codificación de los caracteres
 define("DB_ENCODE","utf8");
